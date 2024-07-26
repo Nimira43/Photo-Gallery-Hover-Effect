e38b2acb424b2de0f1178e7f0fdadf15bca7345f
Nick Rabone <n.rabone72@aol.com>
2024-07-26 01:10:40 +0100

    Vertical scroll of images using JavaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+const navItems = document.querySelectorAll('.nav-item')
+const gallery = document.querySelector('.gallery')
+const images = document.querySelector('.images')
+
+navItems.forEach((navItem, i) => {
+  navItem.addEventListener('mouseover', (e) => {
+    images.style.transform = `translateY(-${i * 25}%)`
+  })
+})
