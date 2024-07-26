58570ec0699feafbf7ec568ef74e184c2845dc7b
Nick Rabone <n.rabone72@aol.com>
2024-07-26 02:04:12 +0100

    Project completed.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const navItems = document.querySelectorAll('.nav-item')
 const gallery = document.querySelector('.gallery')
+const galleryBtn = document.querySelector('.gallery-btn')
 const images = document.querySelector('.images')
 
 navItems.forEach((navItem, i) => {
@@ -13,11 +14,15 @@ navItems.forEach((navItem, i) => {
     let left = e.clientX
     gallery.style.top = `${top}px`
     gallery.style.left = `${left}px`
+    galleryBtn.style.top = `${top}px`
+    galleryBtn.style.left = `${left}px`
   })
   navItem.addEventListener('mouseover', () => {
     gallery.style.transform = 'translate(-50%, -50%) scale(1)'
+    galleryBtn.style.transform = 'translate(-50%, -50%) scale(1)'
   })
   navItem.addEventListener('mouseleave', () => {
     gallery.style.transform = 'translate(-50%, -50%) scale(0)'
+    galleryBtn.style.transform = 'translate(-50%, -50%) scale(0)'
   })
 })
